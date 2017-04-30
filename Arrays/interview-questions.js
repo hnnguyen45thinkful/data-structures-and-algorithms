b4dbf3222223187192729df1f8ad1d62c073463b
Hieu Nguyen <hnnguyen45@gmail.com>
2017-04-29 19:02:27 -0700

    Arrays/interview questions

---
+++ Arrays/interview-questions.js
@@ -0,0 +1,10 @@
+// Interview questions
+// You can use JavaScript's built in arrays rather than your Array object to answer these questions.
+
+//Number 1 Imagine you have an array of numbers. Write an algorithm to remove all numbers less than five from the array.
+
+// Number 2 You shouldn't use the .filter method here; try to write the algorithm from scratch.
+
+// Number 3 Imagine you have two arrays which have already been sorted. Write an algorithm to merge the two arrays into a single array, which should also be sorted. For example, if your input arrays were [1, 3, 6, 8, 11] and [2, 3, 5, 8, 9, 10], your output array should be [1, 2, 3, 3, 5, 6, 8, 8, 9, 10, 11].
+
+// Number 4 Given an array of numbers, write an algorithm to find out the products of every number, except the one at that index. For example, if the input was [1, 3, 9, 4], the output should be [108, 36, 12, 27] (i.e. [3*9*4, 1*9*4, 1*3*4, 1*3*9]).
